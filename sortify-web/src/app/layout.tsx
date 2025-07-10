26ac8ac50f98aad21fb2d3d61f85fdef347870c3
LastSpot <anhminhle402@gmail.com>
2025-07-10 00:02:24 -0500

    feat: add Vercel Analytics for improved user tracking and insights

--- sortify-web/src/app/layout.tsx
+++ sortify-web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,6 +54,7 @@ export default function RootLayout({
         </noscript>
         {children}
         <Toaster position="top-center" expand={true} />
+        <Analytics />
       </body>
     </html>
   );
